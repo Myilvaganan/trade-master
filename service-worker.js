1a598109dc1d5de2e045f7abcc6f67f02a454115
Myilvaganan <65181847+Myilvaganan@users.noreply.github.com>
2026-02-24 11:37:12 +0530

    Added pws

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "techiemaster-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/components/navbar.html",
+  "/css/main.css"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
